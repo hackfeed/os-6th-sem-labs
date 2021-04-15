2b19a269db148dec0151a6fe193de6065d1e4ad4
Sergey Kononenko <hackfeed@yandex.ru>
2021-04-15 10:38:08 +0300

    Remove dmesg log length in run script

--- lab_03/part_01/src/run.sh
+++ lab_03/part_01/src/run.sh
@@ -4,8 +4,8 @@ echo ">>>>> Loading md1"
 insmod md1.ko
 lsmod | grep md
 echo ">>>>> System log after module insert:"
-dmesg | tail -5
+dmesg
 echo ">>>>> Removing md1"
 rmmod md1
 echo ">>>>> System log after module removal:"
-dmesg | tail -5
+dmesg
